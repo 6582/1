7a8ff5a6ab8ed5b21f846af7310e96b24efb441c
lokpro <cellurashin@gmail.com>
2021-08-23 11:44:04 +0800

    Create bs.meta.js

---
+++ bs.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        OPR_brainStorming
+// @namespace   asldufhiu32hr9283hf83123
+// @include     https://wayfarer.nianticlabs.com/*
+// @require     https://cdn.jsdelivr.net/gh/jquery/jquery@3.6.0/dist/jquery.min.js
+// @updateURL    https://github.com/6582/1/raw/main/bs.meta.js
+// @downloadURL  https://github.com/6582/1/raw/main/bs.user.js
+// @version     5.0
+// @run-at document-start
+// @grant       none
+// ==/UserScript==

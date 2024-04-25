04c5cc41691379ed37cce8294bf481eb71fbfc4d
lokpro <2010297+lokpro@users.noreply.github.com>
2024-04-25 16:09:36 +0800

    v6 需要update post URL，舊版本不能再用。

--- bs.meta.js
+++ bs.meta.js
@@ -5,7 +5,7 @@
 // @require     https://cdn.jsdelivr.net/gh/jquery/jquery@3.6.0/dist/jquery.min.js
 // @updateURL    https://github.com/6582/1/raw/main/bs.meta.js
 // @downloadURL  https://github.com/6582/1/raw/main/bs.user.js
-// @version     5.3
+// @version    6.0
 // @run-at document-start
 // @grant       none
 // ==/UserScript==
